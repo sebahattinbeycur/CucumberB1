f5f8f078aec704716602a5dd8e201abd33c76c5b
sebahattinbeycur <beycur@gmail.com>
2023-03-03 21:31:18 +0300

    first commit

---
+++ src/test/java/pages/DashboardPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class DashboardPage extends BaseClass {
+    @FindBy(id = "welcome")
+    public WebElement welcome;       // <== public WebElement welcome = driver.findElement(By.id("welcome"));
+
+    @FindBy(css = "div#branding a img:nth-child(1)")
+    public WebElement dashboardLogo;
+
+    public DashboardPage() {
+        PageFactory.initElements(driver, this);
+    }
+}
